00ad5576944192b6f85a73117c74ca539cf0b4c6
thrall.chen <thrall.chen@gmail.com>
2017-04-20 14:16:42 +0800

    it works

---
+++ src/exceptions/RuntimeException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by Cartman Chen <me@csz.link>.
+ * Author: 陈章--大官人
+ * Github: https://github.com/cartmanchen
+ */
+
+namespace dgr\nohup\exceptions;
+
+class RuntimeException extends \Exception
+{
+
+}
