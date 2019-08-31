3810347e1a15f703e4f5997394b0b33b98258cb5
c00cjz00 <summerhill001@gmail.com>
2019-08-31 09:08:52 +0000

    init

--- 001-submitJob.php
+++ 001-submitJob.php
@@ -1,7 +1,9 @@
 <?php
 require("vendor/autoload.php");
 use dgr\nohup\Nohup;
-$cmd = "sleep 120";
-$process = Nohup::run($cmd);
+$cmd=trim($argv[1]); $outputFile = NULL; $errorFile = NULL;
+if (isset($argv[2])) $outputFile = trim($argv[2]);
+if (isset($argv[3])) $errorFile = trim($argv[3]);
+$process = Nohup::run($cmd,$outputFile,$errorFile);
 $pid = $process->getPid();
 echo $pid;
--- 003-jobDelete.php
+++ 003-jobDelete.php
@@ -3,6 +3,6 @@ require("vendor/autoload.php");
 use dgr\nohup\Process;
 $pid=trim($argv[1]);
 $process = Process::loadFromPid($pid);  
-$status = 0;
-if ($process->isRunning()) $status = 1;
-echo $status;
+if ($process->isRunning()) {
+ $process->stop();
+}
